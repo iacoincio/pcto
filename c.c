69e4f5d8e18d0bd4d8c39fb4d3c085f8b09e99fc
iacoincio <124668958+iacoincio@users.noreply.github.com>
2023-02-07 15:06:53 +0100

    Add files via upload

---
+++ c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+          int a;
+          
+          printf("inserisci la tua eta\n" ); 
+          scanf("%d ", &a);
+          
+          if(a >= 18);
+          {
+            printf("a è maggiorenne\n"); 
+          }
+          else 
+          {
+            printf("a è minorenne\n");
+          }
+
+}
