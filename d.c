fc850f2ed29f4fe505945641c9a974a12a46b8aa
iacoincio <124668958+iacoincio@users.noreply.github.com>
2023-02-07 15:30:56 +0100

    Add files via upload

---
+++ d.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a
+    int b
+    printf("%d %d = &a &b");
+    if("a == b");
+    {
+        printf("a è multiplo di b\n");
+    }
+    else if("a != b")
+    {
+        printf("a non è multiplo di b\n")
+    }
+}
