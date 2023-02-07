37aec3615f914bd8cb0129abae1f9368b97579f4
iacoincio <124668958+iacoincio@users.noreply.github.com>
2023-02-07 17:02:29 +0100

    Add files via upload

---
+++ g.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("inserisci un numero: %d\n", &x);
+    
+    if(x == 4 , 100)
+    {
+        printf("anno non bisestile\n");
+    }
+    else if(x != 4 , 100)
+    {
+        printf("anno bisestile\n");
+    }
+}
