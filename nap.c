5d406daa3f87f3870fe66337a7dac863396c68ec
iacoincio <124668958+iacoincio@users.noreply.github.com>
2023-02-08 16:22:18 +0100

    Add files via upload

---
+++ nap.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    int divisione;
+    printf("0 report terzo giorno pcto coding luiss 8/03/2023\n");
+    printf("1 scrivi un numero intero inversamente ontologico\n");
+    printf("2 giulio sei il miglior souls player\n");
+    printf("3 metodo napoli miglior metodo al mondo,inventato da giulio\n");
+    printf("4 il metodo napoli consiste nello scrivere i numeri direttamente e non farli fare dal terminale\n");
+    printf("5 sono annoiato\n");
+    printf("6 mi hanno chiesto cosa significhi numero ontologico ma non so il significato ma l'insegnante non ci ha creduto\n");
+    printf("7 ho perso troppo sangue per le risate\n");
+    printf("8 federico non si è accorto che la mia mano era sporca di sangue\n");
+    printf("9 video di alessandro barbero decontestualizzato fanno ridere\n");
+    printf("10 dovrei scrivere un programma che prenda un numero N e che verifichi se è primo o meno ma non so come si fa e mi annoio\n");
+    printf("11 federico si merita altre multe per quello che ha insinuato sulla mia mano\n");
+    printf("12 federico pensa che io sia una mente criminale ma in realtà è più criminale di me\n"); 
+    printf("13 vorrei scrivere qualcosa di divertente ma non c'è niente di bello\n");
+    printf("14 padre nostro che sei nei cieli sia santificato il tuo nome venga il tuo regno sia fatta la tua volontà come in cielo cosi in terra dacci oggi il nostro pane quotidiano e rimetti a noi i nostri debiti come noi facciamo con i nostri debitori e non ci indurre in tentazione ma liberaci dal male amen\n");
+    printf("15 finalmente hanno scritto come completare lésercizio deinumeri primi mentre federico inpreca perché non non ci è arrivato lui\n");
+    printf("16 mi stanno chiedendo di fare una sequenza ignota di numeri interi positivi che deve stampare ogni volta la media di tutti i numeri introdotti\n");
+
+}
