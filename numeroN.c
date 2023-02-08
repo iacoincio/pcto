0571af2c531596d3f6ead2d1eb2b377bdca74a61
iacoincio <124668958+iacoincio@users.noreply.github.com>
2023-02-08 15:50:29 +0100

    Add files via upload

---
+++ numeroN.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    int y = x - 1;
+    printf("inserisci un numero\n");
+    scanf("%d", &x);
+    while (x % y != 0 && y > 1)
+    {
+        y = y - 1;
+
+    }
+    
+         if( y == 1)
+        {
+            printf(" è primo\n");
+            
+        }
+        else
+        {
+            printf("non è primo\n");
+
+        }
+        
+    
+}
